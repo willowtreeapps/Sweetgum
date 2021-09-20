6d7814d4e2e430c9b8e15f473b865075b12c5d15
Anthony Iacono <anthony.iacono@willowtreeapps.com>
2021-09-20 13:34:42 -0400

    Fixing some warnings

--- src/Client/Fonts/FontFamilyFactory.cs
+++ src/Client/Fonts/FontFamilyFactory.cs
@@ -7,8 +7,19 @@ using Avalonia.Media;
 
 namespace WillowTree.Sweetgum.Client.Fonts
 {
+    /// <summary>
+    /// Methods which allow simplified convention-based construction of <see cref="FontFamily"/> instances.
+    /// </summary>
     public static class FontFamilyFactory
     {
+        /// <summary>
+        /// Constructs an instance of <see cref="FontFamily"/> for a custom TTF font in the font directory inside the
+        /// assets directory.
+        /// </summary>
+        /// <param name="ttfName">The file name of the TTF file inside the font directory inside the assets directory.</param>
+        /// <param name="familyName">The font family name. This must be correct and match the TTF file details.
+        /// You can use a program like "Font Book" on Mac to determine the family name.</param>
+        /// <returns>An instance of <see cref="FontFamily"/>.</returns>
         public static FontFamily CreateCustom(string ttfName, string familyName)
         {
             return new FontFamily(
